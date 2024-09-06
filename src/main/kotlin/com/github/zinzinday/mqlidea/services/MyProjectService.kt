54c6c2a5ed7f4976d056d57b76f72b7cfa4ac414
GitHub Action <action@github.com>
2024-09-06 15:39:31 +0000

    Template cleanup

--- src/main/kotlin/com/github/zinzinday/mqlidea/services/MyProjectService.kt
+++ src/main/kotlin/com/github/zinzinday/mqlidea/services/MyProjectService.kt
@@ -1,9 +1,9 @@
-package org.jetbrains.plugins.template.services
+package com.github.zinzinday.mqlidea.services
 
 import com.intellij.openapi.components.Service
 import com.intellij.openapi.diagnostic.thisLogger
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
+import com.github.zinzinday.mqlidea.MyBundle
 
 @Service(Service.Level.PROJECT)
 class MyProjectService(project: Project) {
